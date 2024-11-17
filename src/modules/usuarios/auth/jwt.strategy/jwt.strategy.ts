88a938eca0c8adc36b63911dd95418f107cd5364
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-16 23:37:37 -0300

    `Feat: Guard ` | Guard de auth y estrategia passport.

---
+++ src/modules/usuarios/auth/jwt.strategy/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { envs } from 'src/configuration';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: envs.jwt_seed,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username }; // Decodifica el token
+  }
+}
