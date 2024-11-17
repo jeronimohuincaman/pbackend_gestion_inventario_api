43ebdf955f80d94ef20bcd6a06e6bccbab1990ba
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-16 23:02:51 -0300

    `Feat: Dependencias Seguridad` | JWT y Variables de entorno

--- src/configuration/envs.ts
+++ src/configuration/envs.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 // Importamos dependencias
 import 'dotenv/config';
 import * as joi from 'joi';
@@ -6,11 +5,15 @@ import * as joi from 'joi';
 // Creamos una interfaz que se encargue de mejorar el tipado de nuestro codigo
 interface VariablesDeEntorno {
     PORT: number;
+    JWT_SEED: string;
+    DATABASE_URL:string;
 }
 
 // Configuramos JOI
 const envs_schema = joi.object({
     PORT: joi.number().integer().required(), // definimos tipo y si es requerido
+    DATABASE_URL: joi.string().required(), // definimos tipo y si es requerido
+    JWT_SEED: joi.string().required(), // definimos tipo y si es requerido
 }).unknown(true);
 
 /**
@@ -23,5 +26,7 @@ if(error) throw new Error(`Config validation error: ${error.message}`);
 const variables_entorno: VariablesDeEntorno = value;
 
 export const envs = {
-    port: variables_entorno.PORT
+    port: variables_entorno.PORT,
+    database_url: variables_entorno.DATABASE_URL,
+    jwt_seed: variables_entorno.JWT_SEED
 }
